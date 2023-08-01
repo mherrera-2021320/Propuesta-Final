0d14e944535cca291dc3540b95e341e002944cf2
Melvin Josué Herrera Muy <mherrera-2021320@kinal.edu.gt>
2023-08-01 17:15:16 -0600

    numero algo

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://mherrera-2021320.github.io/Propuesta-Final'
+
 })
